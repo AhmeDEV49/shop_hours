da181951ae58ffb675cc491a461dcb60ccc21d87
Ahmed <ahmed@videorunrun.com>
2022-07-22 17:37:27 +0200

    Schema.rb updated

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,11 +12,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_19_150011) do
-  create_table "shops", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.0].define(version: 20_220_719_151_251) do
+  create_table 'shops', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
+  create_table 'time_slots', force: :cascade do |t|
+    t.string 'day'
+    t.time 'start_time'
+    t.time 'end_time'
+    t.integer 'shop_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['shop_id'], name: 'index_time_slots_on_shop_id'
+  end
 end
