54a1ad4b9bea63e8c03bffcfa13e72e3b71df894
Ahmed <ahmed@videorunrun.com>
2022-07-22 17:42:16 +0200

    Add helper method to order week from today + check today?.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def days_week_ordered_from_today
+    today = Date::DAYNAMES.find_index(Date.today.strftime('%A'))
+
+    Date::DAYNAMES[today..].map(&:to_s) + Date::DAYNAMES[0..today - 1]
+  end
+
+  def today?(day:)
+    days_week_ordered_from_today.first == day
+  end
 end
